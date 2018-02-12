8c1090757d62084b667aa1758cb44c223054b557
liujingshi <1471703928@qq.com>
2018-02-12 22:58:11 +0800

    Add sut_jwc_login

---
+++ sut_jwc_login/README.md
@@ -0,0 +1,15 @@
+# 登录SUT教务处
+
+python3.x
+
+需要的库：
+
+pip install requests
+
+pip install Image
+
+需要安装Tesseract-OCR
+
+使用方法:
+
+直接在终端：python sut_jwc_login.py username password
---
+++ sut_jwc_login/sut_jwc_login.py
@@ -0,0 +1,50 @@
+import requests
+from PIL import Image
+from io import BytesIO
+import subprocess
+import re
+import sys
+
+headers = { #请求头数据
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+}
+
+session = requests.session()
+
+def login(UserName, PassWord):
+    response = session.get("http://202.199.96.30/ACTIONVALIDATERANDOMPICTURE.APPPROCESS", headers=headers)
+    image = Image.open(BytesIO(response.content))
+    image.save("tmp.jpg")
+    p = subprocess.Popen("tesseract tmp.jpg tmp", stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
+    p.wait()
+    f = open("tmp.txt", "r")
+    v = f.read().replace("\n", "")
+    v = re.sub("\D", "", v)
+    datas = {
+        "WebUserNO":UserName,
+        "Password":PassWord,
+        "Agnomen":v,
+        "submit.x":"27",
+        "submit.y":"-1"
+    }
+    url = "http://202.199.96.30/ACTIONLOGON.APPPROCESS?mode=4"
+    t = session.post(url, datas, headers=headers)
+    return t.text
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+while True:
+    t = login(username, password);
+    if "请输入正确的附加码" in t:
+        continue
+    if "错误的用户名或者密码" in t:
+        print("User Error")
+        break
+    if "您好!欢迎您登录教务处网络平台" in t:
+        print("Success")
+        break
+
+
